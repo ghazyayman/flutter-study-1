ccdbce374f5f0e484b78bbd8376b1488b0a2c251
ghazyayman <muhghazyayman@gmail.com>
2022-09-24 02:45:22 +0800

    flutter profile app assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,106 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
-import 'package:tif_flutter/nim_rahmat/nim_profile_screen.dart';
+import 'package:flutter_profile_app/about_me.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(ProfileApp());
+
+class ProfileApp extends StatefulWidget {
+  const ProfileApp({super.key});
+
+  @override
+  State<ProfileApp> createState() => _ProfileAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class _ProfileAppState extends State<ProfileApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        home: Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('assets/img/me.jpg'),
+            ),
+            Text(
+              'Ghazy',
+              style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.blueAccent.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 50.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.blueAccent.shade100,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.account_box_rounded,
+                  color: Colors.blueAccent,
+                ),
+                title: Text('D121171307',
+                    style: TextStyle(
+                        color: Colors.blueAccent.shade700,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0)),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.blueAccent,
+                ),
+                title: Text(
+                  'muhghazyayman@gmail.com',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueAccent.shade700,
+                      fontFamily: 'Source Sans Pro'),
+                ),
+              ),
+            ),
+            NavigateButton(name: "About Me", page: AboutMe())
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(
-        children: const [
-          NavigateButton(name: 'Nim-Name', page: NimProfileScreen())
-        ],
-      )),
-    );
+    ));
   }
 }
 
 class NavigateButton extends StatelessWidget {
-
-  const NavigateButton({Key? key, required this.name, required  this.page}) : super(key: key);
+  const NavigateButton({Key? key, required this.name, required this.page})
+      : super(key: key);
 
   final Widget page;
   final String name;
 
-
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton (
+    return ElevatedButton(
       child: Text(name),
       onPressed: () {
         Navigator.push(context, MaterialPageRoute(builder: (ctx) => page));
@@ -65,4 +108,3 @@ class NavigateButton extends StatelessWidget {
     );
   }
 }
-
